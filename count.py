a56722ae1f592f4ac29cb4f95d3c72df25487f0c
apjohnson12 <apjohnson12@crimson.ua.edu>
2023-04-01 02:44:33 -0400

    added mock calculations into the code. Can replace these with our SBLF calculations

--- count.py
+++ count.py
@@ -7,36 +7,38 @@ total_files = 0
 lines_dict = {}
 skip_first_line = True
 
+# first pass to read through all files and calculate counts
 for filename in os.listdir(directory_path):
     file_path = os.path.join(directory_path, filename)
     if os.path.isfile(file_path):
         with open(file_path, 'r') as f:
-            for line in f:
+            for line_num, line in enumerate(f):
                 line = line.strip()  # remove whitespace from beginning and end of line
                 if skip_first_line:
-                    # parse first line for true/false value
                     skip_first_line = False
-                    _, value = line.split(' ', 1)
+                    first_line = line.split(' ', 1)
+                    is_true = True if first_line[1] == 'true' else False
                     continue  # skip the first line
                 if line not in lines_dict:
-                    # if line is not already in lines_dict, add it with a count of 1 and true/false counts
-                    lines_dict[line] = {'count': 1, 'true': 0, 'false': 0}
+                    # if line is not already in lines_dict, add it with counts of 1 and 0
+                    lines_dict[line] = {'true': int(is_true), 'false': int(not is_true), 'total': 1}
                 else:
-                    # if line is already in lines_dict, increment its count and true/false counts
-                    lines_dict[line]['count'] += 1
-                if value.lower() == 'true':
-                    lines_dict[line]['true'] += 1
-                elif value.lower() == 'false':
-                    lines_dict[line]['false'] += 1
+                    # if line is already in lines_dict, increment its counts
+                    lines_dict[line]['true'] += int(is_true)
+                    lines_dict[line]['false'] += int(not is_true)
+                    lines_dict[line]['total'] += 1
                 total_lines += 1
         total_files += 1
         skip_first_line = True  # reset flag for next file
 
+# second pass to calculate percentages and write results to output file
 with open(output_file_path, 'w') as output_file:
     output_file.write(f'Total lines: {total_lines}\n')
     output_file.write(f'Total files: {total_files}\n')
     output_file.write(f'Unique lines: {len(lines_dict)}\n')
     output_file.write('---\n')
     for line, counts in lines_dict.items():
-        output_file.write(f"{line} ({counts['count']} times, {counts['true']} true, {counts['false']} false)\n")
+        true_percentage = counts['true'] / counts['total']
+        false_percentage = counts['false'] / counts['total']
+        output_file.write(f"{line} ({counts['total']} times, {true_percentage:.2%} true, {false_percentage:.2%} false)\n")
 
